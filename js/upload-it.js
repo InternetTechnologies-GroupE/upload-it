28f25fc8ee01eee6e0077e7007f9903e4e71e35a
Benjamin Currie <ben@benjamincurrie.com>
2011-09-22 13:23:41 +0800

    Internet Explorer Fixes

--- js/upload-it.js
+++ js/upload-it.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+	// Fix placeholders
+	if (!("placeholder" in document.createElement("input"))) {
+		$('input, textarea').focus(function() {
+			if($(this).attr('placeholder') && $(this).val()==$(this).attr('placeholder')) {
+				$(this).removeClass('faded').val('');
+			}
+		}).blur(function() {
+			if($(this).attr('placeholder') && ($(this).val()=="" || $(this).val()==$(this).attr('placeholder'))) {
+				$(this).addClass('faded').val($(this).attr('placeholder'));
+			} else {
+				$(this).removeClass('faded');
+			}
+		}).blur();
+	}
+	
+	
 	if($('aside.sticky').length > 0) {
 		sticky_origin = $('aside.sticky').position().top;
 		window.onscroll = function() {
